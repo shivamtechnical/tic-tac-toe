b889e48e572ee3a25ffcd655ddb277e44071e089
shivam technical <shivam8273753651@gmail.com>
2022-11-18 13:07:55 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Board from "./components/board.jsx";
+import History from "./components/History.jsx";
+import { calculateWinner } from "./helpers.js";
+import StatusMessage from "./components/statusMessage.jsx";
 
-function App() {
+import "./styles/root.scss";
+
+const NEW_GAME = [{ board: Array(9).fill(null), isNext: true }];
+const App = () => {
+  const [history, setHistory] = useState(NEW_GAME);
+
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove];
+
+  const { winner, winningSquares } = calculateWinner(current.board);
+
+  const handleSquareClick = (position) => {
+    if (current.board[position] || winner) {
+      return;
+    }
+
+    setHistory((prev) => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((squar, pos) => {
+        if (pos === position) {
+          return last.isXNext ? "X" : "0";
+        }
+
+        return squar;
+      });
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
+    });
+
+    setCurrentMove((prev) => prev + 1);
+  };
+  const moveTo = (move) => {
+    setCurrentMove(move);
+  };
+
+  const onNewGame = () => {
+    setHistory(NEW_GAME);
+    setCurrentMove(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE{" "}
+      </h1>
+      <StatusMessage winner={winner} current={current} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button
+        type="button"
+        onClick={onNewGame}
+        className={`btn-reset ${winner ? "active" : ""}`}
+      >
+        {" "}
+        Start new game
+      </button>
+      <h2 style={{ fontweight: "normal" }}>Current game history</h2>
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls"></div>
     </div>
   );
-}
+};
 
 export default App;
